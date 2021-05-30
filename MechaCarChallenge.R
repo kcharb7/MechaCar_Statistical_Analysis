41515bdc0406831a04851e308335e9efb9562101
kcharb7 <kimberlycharbonneau@live.com>
2021-05-30 17:45:37 -0400

    Adding cleaned code

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,4 @@
-# Linear Regression to Predict MPG
+## Linear Regression to Predict MPG
 
 # Load dplyr package
 library(dplyr)
@@ -12,7 +12,7 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 # Determine the p-value and r-squared value of linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar))
 
-# Summary Statistics on Suspension Coils
+## Summary Statistics on Suspension Coils
 
 # Import and read in the Suspension_Coil.csv file
 SuspensionCoils <- read.csv(file = 'Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
@@ -24,7 +24,7 @@ total_summary <- SuspensionCoils %>% summarize(Mean=mean(PSI), Median=median(PSI
 lot_summary <- SuspensionCoils %>% group_by(Manufacturing_Lot) %>% 
   summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 
-# T-Tests on Suspension Coils
+## T-Tests on Suspension Coils
 
 # Determine if PSI of all manufacturing lots is different from population mean
 t.test(SuspensionCoils$PSI,mu=1500)
